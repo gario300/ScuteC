059b5d7323c9ba243caba07384e4bafc58825bf4
gario300 <41522264+gario300@users.noreply.github.com>
2019-07-26 23:21:23 -0500

    Home

---
+++ components/userbox.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <div class="container">
+            <div class="columns is-centered">
+                <div class="column is-half">
+                    <div class="box">
+                        <article class="media">
+                            <div class="media-left">
+                                <figure class="image is-64x64">
+                                    <img :src="`${yo.avatar}`">
+                                </figure>
+                            </div>
+                        </article>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'userbox'
+        
+    
+
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- pages/home/index.vue
+++ pages/home/index.vue
@@ -1,17 +1,50 @@
 <template>
     <div>
         <navbar></navbar>
+        <div class="container">
+            <div class="columns is-centered">
+                <div class="column is-half">
+                    
+                    <div class="box">
+                        <article class="media">
+                        <div class="media-left">
+                                <figure class="image is-256x256">
+                                    <img id="avatar" class="is-rounded" :src="`${yo.avatar}`">
+                                 </figure>
+                        </div>
+                        <div class="mediacontent">
+                            <div class="content">
+                                <form>
+                                    <div class="columns is-mobile">
+                                        <div class="column is-9">
+                                            <textarea class="textarea is-success is-large" placeholder="Publica algo..."></textarea>
+                                        </div>
+                                        <div class="column">
+                                            <button class="button is-success is-large"><p><i class="far fa-images"></i></p></button>
+                                            <button id="post" class="button is-normal is-success">Post!</button>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                        </article>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
 
     </div>
 </template>
 
 <script>
 import navbar from '../../components/navbar'
+import userbox from '../../components/userbox'
     
     export default {
         middleware: ['auth'],
         components:{
-            navbar
+            navbar, userbox
         },
         data(){
             return{
@@ -33,6 +66,16 @@ import navbar from '../../components/navbar'
     }
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
+#avatar{
+    max-height: 80px !important;
+    max-width: 80px !important;
+    width: 80px !important;
+    height: 80px !important;
+}
+#post{
+    margin-top: 20px !important;
+}
+
 
 </style>
