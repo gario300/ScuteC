34a87f43c71277de023511eca94a037719dacbae
gario300 <41522264+gario300@users.noreply.github.com>
2019-08-09 01:50:27 -0500

    a

--- nuxt.config.js
+++ nuxt.config.js
@@ -88,6 +88,7 @@ export default {
     ** You can extend webpack config here
     */
     extend(config, ctx) {
+     
     }
   }
 }
--- pages/index.vue
+++ pages/index.vue
@@ -101,7 +101,6 @@
 export default {
 
   components: {
-    fail,
   },
 
   data() {
